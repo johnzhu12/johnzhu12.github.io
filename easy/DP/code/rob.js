10dfcdd5c9b7381edecc0f620277a6902ed76d69
johnzhu <1042532367@qq.com>
2020-06-01 18:31:52 +0800

    udapte

---
+++ easy/DP/code/rob.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var rob = function (nums) {
+//     var len = nums.length;
+//     var sumSig = 0, sumDup = 0;
+//     for (var i = 0; i < len; i++) {
+//         if (i % 2 == 0) { //单数,从0开始
+//             sumSig += nums[i]
+//         } else {
+//             sumDup += nums[i]
+//         }
+//     }
+//     return Math.max(sumSig, sumDup)
+// };
+
+var rob = function (nums) {
+
+};
+
+var src = [2, 1, 1, 2]
+var res = rob(src)
+
+console.log(res)
